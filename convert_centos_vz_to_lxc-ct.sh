55259a090d8a62392c4d2846f1230ed78707233c
Lelik13a <lelik.13a@gmail.com>
2015-07-10 14:46:27 +0700

    simple converter

--- convert_centos_vz_to_lxc-ct.sh
+++ convert_centos_vz_to_lxc-ct.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+#
+# template script for converting OpenVZ centos container to LXC
+
+#
+# lxc: linux Container library
+
+# Authors:
+# Daniel Lezcano <daniel.lezcano@free.fr>
+# Ramez Hanna <rhanna@informatiq.org>
+# Fajar A. Nugraha <github@fajar.net>
+# Michael H. Warfield <mhw@WittsEnd.com>
+# and I, with a few changes
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 if [[ x$1 == "x" ]]; then
 	echo "usage: $0 /patch/to/ct_root"
 	exit 0;
